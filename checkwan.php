11bf4c55d2683c6a256f60856e43a29ef87d425e
RickxZen <48241736+RickxZen@users.noreply.github.com>
2021-12-02 10:56:42 +0100

    Update checkwan.php

--- checkwan.php
+++ checkwan.php
@@ -1,61 +1,59 @@
 <?php
+use Transip\Api\Library\TransipAPI;
+use Transip\Api\Library\Entity\Domain\DnsEntry;
 
-//Benodigde API en instellingen (graag nalopen!)
-require_once('lib/Transip/DomainService.php');
-define('DOMAIN', '###');
-
-//TRANSIP Account
-define('USERNAME', '###');
-//API Key
-define('PRIVATE_KEY', '-----BEGIN PRIVATE KEY-----
-==PRIVATE KEY HERE==
------END PRIVATE KEY-----');
+require_once(__DIR__ . '/vendor/autoload.php');
 
 date_default_timezone_set('Europe/Amsterdam');
 
-//Haalt het externe adres op...
+//Your login name on the TransIP website.
+$login = 'username';
+$domainName = 'domain.tld';
+
+//If the generated token should only be usable by whitelisted IP addresses in your Controlpanel
+$generateWhitelistOnlyTokens = false;
+
+//One of your private keys; these can be requested via your Controlpanel
+$privateKey = '-----BEGIN PRIVATE KEY-----
+==PRIVATE KEY HERE==
+-----END PRIVATE KEY-----';
+
+$api = new TransipAPI(
+    $login,
+    $privateKey,
+    $generateWhitelistOnlyTokens
+);
+
+//Create a test connection to the api
+$response = $api->test()->test();
+
+if ($response === true) {
+    echo 'API connection successful!'." \r\n";
+}
+
+//Get external IP address
 $ipAddress = file_get_contents('http://ipecho.net/plain');
-echo "Huidig IP: ".$ipAddress . PHP_EOL;
+echo "Current external IP: ".$ipAddress . PHP_EOL;
 $time = date('Y-m-d H:i:s', time());
-file_put_contents("logging.txt", "". $time ." - Huidig IP ". $ipAddress ." \r\n", FILE_APPEND);
-$newValues = [
-'@' => $ipAddress,
-];
+file_put_contents("logging.txt", "". $time ." - Current external IP ". $ipAddress ." \r\n", FILE_APPEND);
 
-//Verbinden met de TransIP API
-Transip_ApiSettings::$login=USERNAME;
-Transip_ApiSettings::$privateKey= PRIVATE_KEY;
-$dnsEntries = Transip_DomainService::getInfo(DOMAIN)->dnsEntries;
-
-$update = false;
 $newDnsEntries = array();
 
-//nieuwe array opbouwen op basis van oude data
+//Get all DNS records
+$dnsEntries = $api->domainDns()->getByDomainName($domainName);
+
+//Loop through array and update all A records where IP address is old
 foreach($dnsEntries as $dnsEntry){
   array_push($newDnsEntries, $dnsEntry);
-  // @ Wildcard controleren. Als deze verschilt, moeten DNS-instellingen geupdated worden 
-  if (($dnsEntry->type == Transip_DnsEntry::TYPE_A) && ($dnsEntry->name == '@') && $dnsEntry->content != $ipAddress){
-	$dnsEntry->content = $ipAddress;
-	$update=true;
+  if (($dnsEntry->getType() == 'A') && $dnsEntry->getContent() != $ipAddress){
+    //Set new external IP address on record
+	  $dnsEntry->setContent($ipAddress);
+    //Update record with new external IP address
+	  $api->domainDns()->updateEntry($domainName, $dnsEntry);
+    echo "Record changed"." \r\n";
+  	//Logging
+  	$time = date('Y-m-d H:i:s', time());
+  	file_put_contents("logging.txt", "". $time ." - !!UPDATE PERFORMED!! --> New IP: ". $ipAddress ." \r\n", FILE_APPEND);
   }
 }
-
-//Als het IP-adres geupdated moet worden
-if ($update == true ){
-  try{
-	// Voer de update uit...
-	Transip_DomainService::setDnsEntries(DOMAIN, $newDnsEntries);
-	echo "DNS aangepast.";
-	// Stukje logging naar een TXT bestand.
-	$time = date('Y-m-d H:i:s', time());
-	file_put_contents("logging.txt", "". $time ." - !!UPDATE UITGEVOERD!! --> Nieuw IP: ". $ipAddress ." \r\n", FILE_APPEND);
-  }
-  catch(SoapFault $f){
-	// Fout?
-	echo "Er is iets fout gegaan, DNS is niet geupdated... " . $f->getMessage();
-  }
-}
-
-$time = date('Y-m-d H:i:s', time());
-file_put_contents("logging.txt", "". $time ." - Check gedaan --> IP: ". $ipAddress ." \r\n", FILE_APPEND);
 ?>
